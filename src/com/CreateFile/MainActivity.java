4bc0a126ac9fc08c4ea06cd3e55fdd14b278a837
kyle kersey <kylelk@comcast.net>
2013-09-09 13:14:48 -0700

    CreateFile

--- src/com/CreateFile/MainActivity.java
+++ src/com/CreateFile/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import java.io.*;
+import android.content.*;
+
 
 public class MainActivity extends Activity
 {
@@ -14,4 +17,31 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	
+	public void saveButton(View v) throws FileNotFoundException{
+		EditText textbodyField = (EditText) findViewById(R.id.textbody_field);
+		String textbody = textbodyField.getText().toString();
+		EditText filePath = (EditText) findViewById(R.id.file_path);
+		String FilePath = filePath.getText().toString();
+		
+		if(FilePath == null){
+			Context context = getApplicationContext(); 
+			CharSequence text = "Please enter file path";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration); 
+			toast.show();
+		} 
+		else
+		{
+			PrintWriter writer = new PrintWriter(FilePath); 
+			writer.println(textbody);  
+			writer.close();
+
+			Context context = getApplicationContext(); 
+			CharSequence text = FilePath;
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration); 
+			toast.show();
+		}
+	}
 }
